38d9d9f6503bbcf12c85a3828ec82aa0fd140b85
ashrafulrifaz <developerashrafulislamrifaz@gmail.com>
2023-11-12 22:17:13 +0600

    added error page

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,13 @@
+import { Link } from 'react-router-dom';
+import errorImage from '../../../assets/icon/404.gif'
 
 const ErrorPage = () => {
     return (
-        <div>
-            
+        <div className='text-center py-5'>
+            <img src={errorImage} alt="404" className='w-1/2 mx-auto' />
+            <Link to="/">
+                <button className='bg-[#E8E8E8] text-[#BB8506] py-1.5 px-5 rounded-md border-b-2 border-[#BB8506] hover:scale-95 transition-all'>Back to home</button>
+            </Link>
         </div>
     );
 };
